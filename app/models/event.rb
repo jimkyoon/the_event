3b8c28dde878285e8af30065152c60f7885e3ec4
Jim Yoon <jimyoon90@gmail.com>
2017-10-07 23:09:47 -0700

    fixed sql delete problem

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :user
   has_many :attendees
-  has_many :items
+  has_many :items, dependent: :destroy
   has_many :comments
 
   validates :name, :description, :when, :where, presence:true
@@ -10,7 +10,7 @@ class Event < ActiveRecord::Base
   geocoded_by :where
   after_validation :geocode
 
-  #grab how many attendees are at this event 
+  #grab how many attendees are at this event
   def attendee_count
     self.attendees.count
   end
